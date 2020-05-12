98086f2cd6abb604c79820eee1938650aa25ee2b
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-11 22:36:23 -0400

    created components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Avatar from 'avataaars'
+import React, { Fragment } from 'react';
+import Home from './components/Home';
+// import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Avatar
-          avatarStyle='Circle'
-          topType='ShortHairShortWaved'
-          accessoriesType='Prescription02'
-          hairColor='Black'
-          facialHairType='BeardLight'
-          facialHairColor='Black'
-          clotheType='BlazerShirt'
-          eyeType='Default'
-          eyebrowType='Default'
-          mouthType='Default'
-          skinColor='Brown'
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Home />
+    </Fragment>
   );
 }
 
