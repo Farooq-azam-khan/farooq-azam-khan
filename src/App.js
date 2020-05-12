e0ac3014eabfac8e674f62da90498282fe0e756c
farooq-azam-khan <khanazamfarooq99@gmail.com>
2020-05-11 22:14:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Avatar from 'avataaars'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Avatar
+          avatarStyle='Circle'
+          topType='ShortHairShortWaved'
+          accessoriesType='Prescription02'
+          hairColor='Black'
+          facialHairType='BeardLight'
+          facialHairColor='Black'
+          clotheType='BlazerShirt'
+          eyeType='Default'
+          eyebrowType='Default'
+          mouthType='Default'
+          skinColor='Brown'
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
