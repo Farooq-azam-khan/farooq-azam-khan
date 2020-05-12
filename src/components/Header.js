ad0ed707d64c02ab5186e39624bec2f9df52575c
farooq-azam-khan <khanazamfarooq99@gmail.com>
2020-05-11 23:08:42 -0400

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,9 @@
 import React from 'react'
 
-import Avatar from 'avataaars'
 export default function Header() {
     return (
         <div>
-            <Avatar
-                avatarStyle='Circle'
-                topType='ShortHairShortWaved'
-                accessoriesType='Prescription02'
-                hairColor='Black'
-                facialHairType='BeardLight'
-                facialHairColor='Black'
-                clotheType='BlazerShirt'
-                eyeType='Default'
-                eyebrowType='Default'
-                mouthType='Default'
-                skinColor='Brown'
-            />
+            here from header
         </div>
     )
 }
