407e368c9c95679f4c7964235babc7357638010a
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-05-12 00:31:33 -0400

    added particles effect to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,77 @@
-import React from 'react'
+import React, { Fragment } from 'react';
 
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/core/styles';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+
+const useStyles = makeStyles({
+  particlesCanvas: {
+    position: 'absolute',
+  },
+});
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar />
-            <Header />
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Navbar />
+      <Header />
+      <Particles
+        canvasClassName={classes.particlesCanvas}
+        params={{
+          particles: {
+            number: {
+              value: 25,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 1,
+                color: lightBlue[900],
+              },
+            },
+            size: {
+              value: 2,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 10,
+                size_min: 0.1,
+                sync: true,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true,
+              },
+            },
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: lightBlue[500], //'#3CA9D1',
+                blur: 1,
+              },
+            },
+          },
+        }}
+        style={{
+          width: '100%',
+        }}
+      />
+    </Fragment>
+  );
+};
 
-export default Home
+export default Home;
