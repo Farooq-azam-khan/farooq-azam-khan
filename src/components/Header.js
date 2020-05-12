1f4d889913083558591263064033fa427ab5dda6
farooq-azam-khan <khanazamfarooq99@gmail.com>
2020-05-11 22:36:23 -0400

    created components

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Avatar from 'avataaars'
+export default function Header() {
+    return (
+        <div>
+            <Avatar
+                avatarStyle='Circle'
+                topType='ShortHairShortWaved'
+                accessoriesType='Prescription02'
+                hairColor='Black'
+                facialHairType='BeardLight'
+                facialHairColor='Black'
+                clotheType='BlazerShirt'
+                eyeType='Default'
+                eyebrowType='Default'
+                mouthType='Default'
+                skinColor='Brown'
+            />
+        </div>
+    )
+}
