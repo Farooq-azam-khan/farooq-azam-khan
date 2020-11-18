afc6fa46683c0418d7aa9f239f0be8c69fc9f5a1
farooq-azam-khan <khanazamfarooq99@gmail.com>
2020-11-17 21:00:26 -0500

    added tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
-function HomePage() {
-    return <div>Farooq Azam Khan</div>
-}
+import Head from 'next/head'
 
-export default HomePage
+export default function Homepage() {
+    return (<main className="bg-gray-100 mt-10 text-xl">
+        <Head><title>Farooq Azam Khan | Portfolio</title></Head>
+        <h1>Farooq Azam Khan</h1>
+    </main>)
+}
