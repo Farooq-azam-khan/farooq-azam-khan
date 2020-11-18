6ed9221033d02a3ed67215f7ce64776281ac8696
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-11-17 21:58:20 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import * as React from 'react'
 export default function Homepage() {
     return (<main className="bg-gray-100 mt-10 text-xl">
         <Head><title>Farooq Azam Khan | Portfolio</title></Head>
