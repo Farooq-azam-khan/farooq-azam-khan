3e56c81ad91b40a082595c7811fa7e51a48599fd
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2020-12-25 20:18:01 -0500

    hard reset

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Head from 'next/head'
-import * as React from 'react'
-export default function Homepage() {
-    return (<main className="bg-gray-100 mt-10 text-xl">
-        <Head><title>Farooq Azam Khan | Portfolio</title></Head>
-        <h1>Farooq Azam Khan</h1>
-    </main>)
-}
