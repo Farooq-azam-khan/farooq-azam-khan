ffdade54e6fc5291bcf9962b7ef71e1639e08da1
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2020-12-25 20:59:34 -0500

    language section

--- src/Types.elm
+++ src/Types.elm
@@ -2,7 +2,14 @@ module Types exposing (..)
 
 
 type alias Model =
-    {}
+    { languages : List Language
+    }
+
+
+type alias Language =
+    { href : String
+    , name : String
+    }
 
 
 type Msg
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './src/**/*.elm'
+    , './src/**/*.js'
+    , './src/**/*.jsx'
+    , './src/**/*.ts'
+    , './src/**/*.tsx'
+    , './src/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
