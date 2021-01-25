7a57d66b237fc5ba1d9646a4aa14e324f9c1f29d
farooq-azam-khan <khanazamfarooq99@gmail.com>
2021-01-25 10:48:38 -0500

    mobile to sm portfolio styles done (need to center cards on small screen and create an elm model for reusability

--- src/Main.elm
+++ src/Main.elm
@@ -59,17 +59,18 @@ view model =
     div [ class "bg-gray-100 pb-32" ]
         [ navigation model
         , div [ class "px-3 mt-20" ]
-            [ h1 [ class "text-5xl text-3xl font-semibold" ] [ text "Full Stack Developer with a passion for machine learning" ]
+            [ h1 [ class "text-5xl text-3xl font-semibold" ]
+                [ text "Full Stack Developer with a passion for machine learning" ]
             , div [ class "space-y-6 flex flex-col mt-20 md:flex-row md:items-center md:space-x-10 md:space-y-0" ]
                 [ div []
                     [ button
-                        [ class "w-full md:px-10 md:py-5 transition duration-500 ease-in-out text-xl tracking-wider font-semibold bg-gray-900 text-gray-100 px-3 py-3 shadow-lg rounded-lg hover:bg-gray-800 motion-reduce:transition-none motion-reduce:transform-none"
+                        [ class "w-full md:px-10 md:py-5 transition duration-500 ease-in-out text-xl tracking-wider font-semibold bg-indigo-700 text-gray-100 px-3 py-3 shadow-lg rounded-lg hover:bg-indigo-600 motion-reduce:transition-none motion-reduce:transform-none"
                         ]
                         [ text "Portfolio" ]
                     ]
                 , div []
                     [ button
-                        [ class "w-full md:px-10 md:py-5 transition duration-500 ease-in-out text-xl tracking-wider font-semibold bg-gray-400 text-gray-900  px-3 py-3 shadow-lg rounded-lg hover:bg-gray-500 motion-reduce:transition-none motion-reduce:transform-none"
+                        [ class "w-full px-3 py-3 md:px-10 md:py-5 transition duration-500 ease-in-out text-xl tracking-wider font-semibold bg-indigo-200 text-indigo-800  hover:bg-indigo-400  shadow-lg rounded-lg  motion-reduce:transition-none motion-reduce:transform-none"
                         ]
                         [ text "Blogs" ]
                     ]
@@ -183,10 +184,131 @@ sm_form_section =
 
 portfolio_section : Html Msg
 portfolio_section =
-    div [ class "mt-32 bg-gray-900 text-white py-3 px-3" ]
-        [ span [ class "uppercase tracking-wider font-semibold py-1 rounded-full" ] [ text "portfolio" ]
-        , div [ class "text-white" ] [ text "todo" ]
+    div [ class "relative mt-32" ]
+        [ div [ class "absolute inset-0 bg-indigo-700" ] [ text "" ]
+        , div [ class "relative z-10 mt-32 text-white py-3 px-3 grid gap-y-32 grid-cols-1 " ]
+            [ my_songify_card
+            , scheming_in_haskell_card
+            , jarvis_the_typographer_card
+            ]
         ]
+
+
+my_songify_img : String -> Html Msg
+my_songify_img className =
+    img
+        [ class className
+        , src "https://images.unsplash.com/photo-1532354058425-ba7ccc7e4a24?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
+        ]
+        []
+
+
+my_songify_card : Html Msg
+my_songify_card =
+    div [ class "max-w-2xl bg-white flex flex-col items-center rounded-lg shadow-xl py-5 space-y-10" ]
+        [ div
+            -- Image
+            []
+            [ my_songify_img "w-full rounded-lg shadow-lg -mt-20" ]
+        , div
+            -- tags
+            [ class "flex space-x-3" ]
+            [ lang_tag "python"
+            , lang_tag "react/js"
+            ]
+        , div
+            -- content
+            [ class "flex flex-col items-center max-w-xl space-y-5" ]
+            [ h2 [ class "text-4xl text-center font-bold text-indigo-700" ] [ text "My Songify" ]
+            , p [ class "text-gray-700 text-lg text-center" ] [ text "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione." ]
+            ]
+        , div
+            -- links
+            [ class "flex space-x-3"
+            ]
+            [ code_btn "#", live_demo_btn "#" ]
+        ]
+
+
+scheming_in_haskell_card : Html Msg
+scheming_in_haskell_card =
+    div [ class "max-w-2xl bg-white flex flex-col items-center rounded-lg shadow-xl py-5 space-y-10" ]
+        [ div
+            -- Image
+            []
+            [ my_songify_img "w-full rounded-lg shadow-lg -mt-20" ]
+        , div
+            -- tags
+            [ class "flex space-x-3" ]
+            [ lang_tag "python"
+            , lang_tag "react/js"
+            ]
+        , div
+            -- content
+            [ class "flex flex-col items-center max-w-xl space-y-5" ]
+            [ h2 [ class "text-4xl text-center font-bold text-indigo-700" ]
+                [ text "Scheming in Haskell" ]
+            , p [ class "text-gray-700 text-lg text-center" ]
+                [ text "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione." ]
+            ]
+        , div
+            -- links
+            [ class "flex space-x-3"
+            ]
+            [ code_btn "#", live_demo_btn "#" ]
+        ]
+
+
+jarvis_the_typographer_card : Html Msg
+jarvis_the_typographer_card =
+    div [ class "max-w-2xl bg-white flex flex-col items-center rounded-lg shadow-xl py-5 space-y-10" ]
+        [ div
+            -- Image
+            []
+            [ my_songify_img "w-full rounded-lg shadow-lg -mt-20" ]
+        , div
+            -- tags
+            [ class "flex space-x-3" ]
+            [ lang_tag "python"
+            , lang_tag "react/js"
+            ]
+        , div
+            -- content
+            [ class "flex flex-col items-center max-w-xl space-y-5" ]
+            [ h2 [ class "text-4xl text-center font-bold text-indigo-700" ]
+                [ text "Jarvis the Typogropher" ]
+            , p [ class "text-gray-700 text-lg text-center" ]
+                [ text "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione." ]
+            ]
+        , div
+            -- links
+            [ class "flex space-x-3"
+            ]
+            [ code_btn "#", live_demo_btn "#" ]
+        ]
+
+
+lang_tag : String -> Html Msg
+lang_tag tag_nm =
+    span [ class "inline-flex items-center text-indigo-900 bg-indigo-200 px-4 py-1 rounded-full" ] [ text tag_nm ]
+
+
+code_btn : String -> Html Msg
+code_btn gh_lnk =
+    a
+        [ href gh_lnk
+        , class "text-white text-lg rounded-lg shadow-lg px-5 py-2 bg-indigo-700"
+        ]
+        [ text "Code" ]
+
+
+live_demo_btn : String -> Html Msg
+live_demo_btn dmo_lnk =
+    a
+        [ href dmo_lnk
+        , class "border text-lg rounded-lg shadow-lg  px-3 py-2  text-indigo-700 border-indigo-900"
+        ]
+        [ text "Live Demo" ]
 
 
 navigation : Model -> Html Msg
@@ -210,12 +332,12 @@ navigation model =
 
 sm_navbar_modal : Html Msg
 sm_navbar_modal =
-    div [ class "transition easy-in-out duration-500 absolute rounded-lg top-0 left-0 w-full my-1 bg-gray-900 shadow-xl motion-reduce:transition-none motion-reduce:transform-none" ]
+    div [ class "transition easy-in-out duration-500 absolute rounded-lg top-0 left-0 w-full my-1 bg-indigo-700 shadow-xl motion-reduce:transition-none motion-reduce:transform-none" ]
         [ div [ class "flex items-center justify-between px-4 pt-6 pb-3" ]
-            [ div [ class "text-gray-100 tracking-wider font-semibold" ] [ text "Farooq A. Khan" ]
+            [ div [ class "text-indigo-200 font-bold text-lg tracking-wider font-semibold" ] [ text "Farooq A. Khan" ]
             , button
                 [ onClick ToggleSmNavbar
-                , class "text-white hover:bg-gray-300 hover:text-gray-700 px-1 py-1 rounded-lg"
+                , class "text-white hover:bg-indigo-300 hover:text-indigo-600 px-1 py-1 rounded-lg"
                 ]
                 [ close "w-8 h-8" ]
             ]
@@ -230,4 +352,4 @@ sm_navbar_modal =
 
 sm_nav_link_item : String -> String -> Html Msg
 sm_nav_link_item name link_to =
-    li [ class "hover:text-gray-400" ] [ a [ href link_to ] [ text name ] ]
+    li [ class "font-bold text-xl hover:text-indigo-200" ] [ a [ href link_to ] [ text name ] ]
