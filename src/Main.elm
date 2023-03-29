4bd2e2fc3799de6bf4c95c9fd71f122aa17d45c8
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-03-29 11:01:06 -0400

    removed dark mode, gradient for section headers, removed programming seciton

--- src/Main.elm
+++ src/Main.elm
@@ -50,9 +50,9 @@ update msg model =
 
 blog_card : { blog_title : String, blog_description : String, blog_link : String } -> Html Msg
 blog_card { blog_title, blog_description, blog_link } =
-    div [ class "shadow-xl max-w-sm p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700" ]
-        [ h3 [ class "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" ] [ text blog_title ]
-        , p [ class "mb-3 font-normal text-gray-700 dark:text-gray-400" ] [ text blog_description ]
+    div [ class "shadow-2xl max-w-sm p-6 bg-gray-100 border border-gray-200 rounded-lg " ]
+        [ h3 [ class "mb-2 text-2xl font-bold tracking-tight text-gray-900 " ] [ text blog_title ]
+        , p [ class "mb-3 font-normal text-gray-700 " ] [ text blog_description ]
         , a
             [ href blog_link
             , target "blank_"
@@ -79,21 +79,22 @@ blog_card { blog_title, blog_description, blog_link } =
 interest_card : { interest_title : String, interest_detail : String } -> Html Msg
 interest_card { interest_title, interest_detail } =
     div
-        [ class "flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800 "
+        [ class "flex flex-col justify-between p-4 leading-normal"
         ]
-        [ div
-            [ class "flex flex-col justify-between p-4 leading-normal"
+        [ h3
+            [ class "mb-2 text-xl font-bold tracking-tight text-gray-900 "
             ]
-            [ h3
-                [ class "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                ]
-                [ text interest_title ]
-            , p
-                [ class "mb-3 font-normal text-gray-700 dark:text-gray-400"
-                ]
-                [ text interest_detail ]
+            [ text interest_title ]
+        , p
+            [ class "mb-3 text-lg font-normal text-gray-700"
             ]
+            [ text interest_detail ]
         ]
+
+
+section_gradient : String
+section_gradient =
+    "text-transparent bg-clip-text bg-gradient-to-r from-red-400 via-indigo-500 to-red-500"
 
 
 view : Model -> Browser.Document Msg
@@ -104,37 +105,40 @@ view _ =
             [ margin_content_component
                 [ div [ class "" ]
                     [ div [ class "pt-20 flex items-center justify-between" ]
-                        [ h2 [ class "text-indigo-700 text-xl md:text-4xl  font-extralight" ]
+                        [ h2 [ class "text-indigo-700 text-2xl md:text-4xl  font-extralight" ]
                             [ text "Farooq A. Khan"
                             ]
                         , div
-                            [ class "flex items-center  space-x-2" ]
+                            [ class "flex items-center space-x-2" ]
                             [ a [ target "_blank", href "https://github.com/Farooq-azam-khan" ] [ github_icon ]
                             , a [ target "_blank", href "https://www.linkedin.com/in/farooq-khan-46167bb5/" ]
                                 [ img [ class "w-8 h-8", src "/linkedin-logo-512.webp" ] []
                                 ]
                             ]
                         ]
-                    , h1 [ class " text-indigo-500 text-4xl md:text-7xl font-bold" ] [ text "Software & Machine Learning Engineer" ]
+                    , h1 [ class "text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-red-500 text-5xl md:text-7xl font-bold" ] [ text "Software & Machine Learning Engineer" ]
                     , p
-                        [ class "mt-10 md:mt-24 text-md md:text-xl italic leading-loose tracking-wider lg:max-w-3xl " ]
+                        [ class "mt-14 md:mt-32 text-md md:text-xl italic leading-loose tracking-wider lg:max-w-3xl " ]
                         [ text "Machine Learning Researcher interested in "
                         , ul
-                            [ class "grid grid-cols-1 md:grid-cols-2 gap-5 mt-5" ]
-                            [ li [ class "text-indigo-500 rounded-md flex justify-center items-center" ]
-                                [ interest_card { interest_title = "Artificial Intelligence", interest_detail = "Machine Learning, Reinforcement Learning, NLP, MLOps." }
-                                ]
+                            [ class "grid grid-cols-1 md:grid-cols-2 md:gap-5 mt-5" ]
+                            [ interest_card { interest_title = "Artificial Intelligence", interest_detail = "Machine Learning, Reinforcement Learning, NLP, MLOps." }
                             , interest_card { interest_title = "Software Engineering", interest_detail = "Prompt Egineering, Backend Development, Rust, Python, etc." }
                             , interest_card { interest_title = "Functional Programming", interest_detail = "Category Thoery, Elm Programming, Haskell, etc." }
                             , interest_card { interest_title = "Mathematics", interest_detail = "Fourier Analysis, Linear Albegra, Statistics, Graph Thoery, etc. " }
                             ]
                         ]
-                    , div [ class " mt-32 rounded-md" ]
-                        [ h2 [ class "text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
+                    , div [ class " mt-32 md:mt-64 rounded-md" ]
+                        [ h2 [ class <| section_gradient ++ " text-xl font-semibold tracking-wide uppercase" ]
                             [ text "Blog Highlights" ]
-                        , div [ class "mt-14 grid grid-cols-1 md:grid-cols-2 gap-10 " ]
-                            [ blog_card { blog_title = "Term Frequency-Inverse Document Frequency", blog_description = "In this tutorial we will look at what TF and IDF are and how they can be use to process text data in Machine learning.", blog_link = "https://blog.farooqkhan.ca/tfidf" }
-                            , blog_card { blog_title = "Large Scale Vector Comparison", blog_description = "In this post, we will look at the quora qna dataset and aim to encode and compare all question pairs. The purpose of is to look at a real dataset.", blog_link = "https://blog.farooqkhan.ca/cosine-similarity-pt-2" }
+                        , div [ class "mt-5 md:mt-14 grid grid-cols-1 md:grid-cols-2 gap-y-14 md:gap-x-5 " ]
+                            [ blog_card
+                                { blog_title = "Term Frequency-Inverse Document Frequency", blog_description = "In this tutorial we will look at what TF and IDF are and how they can be use to process text data in Machine learning.", blog_link = "https://blog.farooqkhan.ca/tfidf" }
+                            , blog_card
+                                { blog_title = "Large Scale Vector Comparison"
+                                , blog_description = "In this post, we will look at the quora qna dataset and aim to encode and compare all question pairs. The purpose of is to look at a real dataset."
+                                , blog_link = "https://blog.farooqkhan.ca/cosine-similarity-pt-2"
+                                }
                             , blog_card
                                 { blog_title = "Comparing Vectors with Cosine Simlarity Function"
                                 , blog_description = "This tutorial will focus on the math behind text vector similarity using numpy, pytorch, and stentence-transformers libraries in python."
@@ -144,9 +148,9 @@ view _ =
                         ]
                     ]
                 ]
-            , section [ class "mt-64 " ]
+            , section [ class "mt-32 md:mt-64 " ]
                 [ margin_content_component
-                    [ h2 [ class "text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
+                    [ h2 [ class <| section_gradient ++ " text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
                         [ text "Personal Projects" ]
                     , ol [ class "mt-14 grid sm:grid-cols-2 lg:grid-cols-4 gap-2 sm:gap-5" ]
                         [ li [ class "space-x-3 flex items-center " ]
@@ -176,38 +180,39 @@ view _ =
                     , personal_project_component { project_name = "Chat App", github_link = "https://github.com/Farooq-azam-khan/chat-app-elm" }
                     ]
                 ]
-            , section [ class "mt-64" ]
+
+            -- , section [ class "mt-64" ]
+            --     [ margin_content_component
+            --         [ h2 [ class <| section_gradient ++ " text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
+            --             [ text "Programming Languages and Tools" ]
+            --         , ol [ class "mt-14 grid gap-2 sm:gap-5 sm:grid-cols-2 lg:grid-cols-4 list-disc" ]
+            --             [ li []
+            --                 [ text "python"
+            --                 , ol [ class "list-disc ml-5" ]
+            --                     [ li [] [ text "numpy" ]
+            --                     , li [] [ text "pandas" ]
+            --                     , li [] [ text "matplotlib" ]
+            --                     , li [] [ text "pytorch" ]
+            --                     , li [] [ text "seaborn" ]
+            --                     ]
+            --                 ]
+            --             , li []
+            --                 [ text "elm"
+            --                 , ol [ class "list-disc ml-5" ] [ li [] [ text "elm/graphql" ] ]
+            --                 ]
+            --             , li []
+            --                 [ text "graphql / hasura"
+            --                 ]
+            --             , li [] [ text "Rust" ]
+            --             , li [] [ text "Haskell" ]
+            --             , li [] [ text "C" ]
+            --             , li [] [ text "PostgreSql/Sqllite/pocket db" ]
+            --             ]
+            --         ]
+            --     ]
+            , section [ class "mt-32 md:my-64" ]
                 [ margin_content_component
-                    [ h2 [ class "text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
-                        [ text "Programming Languages and Tools" ]
-                    , ol [ class "mt-14 grid gap-2 sm:gap-5 sm:grid-cols-2 lg:grid-cols-4 list-disc" ]
-                        [ li []
-                            [ text "python"
-                            , ol [ class "list-disc ml-5" ]
-                                [ li [] [ text "numpy" ]
-                                , li [] [ text "pandas" ]
-                                , li [] [ text "matplotlib" ]
-                                , li [] [ text "pytorch" ]
-                                , li [] [ text "seaborn" ]
-                                ]
-                            ]
-                        , li []
-                            [ text "elm"
-                            , ol [ class "list-disc ml-5" ] [ li [] [ text "elm/graphql" ] ]
-                            ]
-                        , li []
-                            [ text "graphql / hasura"
-                            ]
-                        , li [] [ text "Rust" ]
-                        , li [] [ text "Haskell" ]
-                        , li [] [ text "C" ]
-                        , li [] [ text "PostgreSql/Sqllite/pocket db" ]
-                        ]
-                    ]
-                ]
-            , section [ class "my-64" ]
-                [ margin_content_component
-                    [ h2 [ class "text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
+                    [ h2 [ class <| section_gradient ++ " text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
                         [ text "Book Recommendations" ]
                     , ol [ class "mt-14 grid gap-2 sm:gap-5 sm:grid-cols-2 lg:grid-cols-3 list-disc" ]
                         -- potential obj: image url, book name, author
