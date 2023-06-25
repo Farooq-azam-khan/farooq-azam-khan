2f8821db95849d5541b8e57c035ca1e4829c2439
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-06-25 15:51:57 -0400

    ported over Index.elm + delete unused routes

--- app/Site.elm
+++ app/Site.elm
@@ -8,7 +8,7 @@ import SiteConfig exposing (SiteConfig)
 
 config : SiteConfig
 config =
-    { canonicalUrl = "https://elm-pages.com"
+    { canonicalUrl = "https://farooqkhan.ca"
     , head = head
     }
 
