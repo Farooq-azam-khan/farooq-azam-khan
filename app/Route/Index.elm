148adbeb0784fd9c1b8191911e911c39a6bca923
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-06-25 15:51:57 -0400

    ported over Index.elm + delete unused routes

--- app/Route/Index.elm
+++ app/Route/Index.elm
@@ -4,13 +4,16 @@ import BackendTask exposing (BackendTask)
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo as Seo
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Pages.Url
 import PagesMsg exposing (PagesMsg)
-import UrlPath
 import Route
 import RouteBuilder exposing (App, StatelessRoute)
 import Shared
+import Svg exposing (path)
+import Svg.Attributes as SvgAttr
+import UrlPath
 import View exposing (View)
 
 
@@ -57,32 +60,288 @@ head :
 head app =
     Seo.summary
         { canonicalUrlOverride = Nothing
-        , siteName = "elm-pages"
+        , siteName = "farooq-a-khan-portfolio"
         , image =
             { url = [ "images", "icon-png.png" ] |> UrlPath.join |> Pages.Url.fromPath
-            , alt = "elm-pages logo"
+            , alt = "idk..."
             , dimensions = Nothing
             , mimeType = Nothing
             }
-        , description = "Welcome to elm-pages!"
+        , description = "Personal/Portfolio site |> Farooq A. Khan"
         , locale = Nothing
-        , title = "elm-pages is running"
+        , title = "Farooq Khan"
         }
         |> Seo.website
-
 
 view :
     App Data ActionData RouteParams
     -> Shared.Model
     -> View (PagesMsg Msg)
 view app shared =
-    { title = "elm-pages is running"
+    { title = "Farooq Azam Khan | Home"
     , body =
-        [ Html.h1 [] [ Html.text "elm-pages is up and running!" ]
-        , Html.p []
-            [ Html.text <| "The message is: " ++ app.data.message
+        [ main_ [ class "font-roboto font-sans mb-10" ]
+            [ margin_content_component
+                [ div [ class "" ]
+                    [ div [ class "pt-20 flex items-center justify-between" ]
+                        [ h2 [ class "text-indigo-700 text-2xl md:text-4xl  font-extralight" ]
+                            [ text "Farooq A. Khan"
+                            ]
+                        , div
+                            [ class "flex items-center space-x-2" ]
+                            [ a [ target "_blank", href "https://github.com/Farooq-azam-khan" ] [ github_icon ]
+                            , a [ target "_blank", href "https://www.linkedin.com/in/farooq-khan-46167bb5/" ]
+                                [ img [ class "w-8 h-8", src "/linkedin-logo-512.webp" ] []
+                                ]
+                            ]
+                        ]
+                    , h1 [ class "text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-red-500 text-5xl md:text-7xl font-bold" ] [ text "Software & Machine Learning Engineer" ]
+                    , p
+                        [ class "mt-14 md:mt-32 text-md md:text-xl italic leading-loose tracking-wider lg:max-w-3xl " ]
+                        [ text "Machine Learning Researcher interested in "
+                        , ul
+                            [ class "grid grid-cols-1 md:grid-cols-2 md:gap-5 mt-5" ]
+                            [ interest_card { interest_title = "Artificial Intelligence", interest_detail = "Machine Learning, Reinforcement Learning, NLP, MLOps." }
+                            , interest_card { interest_title = "Software Engineering", interest_detail = "Prompt Egineering, Backend Development, Rust, Python, etc." }
+                            , interest_card { interest_title = "Functional Programming", interest_detail = "Category Thoery, Elm Programming, Haskell, etc." }
+                            , interest_card { interest_title = "Mathematics", interest_detail = "Fourier Analysis, Linear Albegra, Statistics, Graph Thoery, etc. " }
+                            ]
+                        ]
+                    , div [ class " mt-32 md:mt-64 rounded-md" ]
+                        [ h2 [ class <| section_gradient ++ " text-xl font-semibold tracking-wide uppercase" ]
+                            [ text "Blog Highlights" ]
+                        , div
+                            [ class "mt-5 md:mt-14 grid grid-cols-1 gap-y-20" ]
+                            [ blog_card
+                                { blog_title = "Term Frequency-Inverse Document Frequency"
+                                , blog_description = "In this tutorial we will look at what TF and IDF are and how they can be use to process text data in Machine learning."
+                                , blog_link = "https://blog.farooqkhan.ca/tfidf"
+                                }
+                            , blog_card
+                                { blog_title = "Large Scale Vector Comparison"
+                                , blog_description = "In this post, we will look at the quora qna dataset and aim to encode and compare all question pairs. The purpose of is to look at a real dataset."
+                                , blog_link = "https://blog.farooqkhan.ca/cosine-similarity-pt-2"
+                                }
+                            , blog_card
+                                { blog_title = "Comparing Vectors with Cosine Simlarity Function"
+                                , blog_description = "This tutorial will focus on the math behind text vector similarity using numpy, pytorch, and stentence-transformers libraries in python."
+                                , blog_link = "https://blog.farooqkhan.ca/cosine-similarity"
+                                }
+                            ]
+                        ]
+                    ]
+                ]
+            , personal_project_list_component
+
+            -- , section [ class "mt-32 md:my-64" ]
+            --     [ margin_content_component
+            --         [ h2 [ class <| section_gradient ++ " text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
+            --             [ text "Book Recommendations" ]
+            --         , ol [ class "mt-14 grid gap-2 sm:gap-5 sm:grid-cols-2 lg:grid-cols-3 list-disc" ]
+            --             -- potential obj: image url, book name, author
+            --             [ li [] [ text "Elm in Action (difficulty: easy)" ]
+            --             , li [] [ text "Category Theory for Programmgers (difficulty: hard)" ]
+            --             , li [] [ text "Deep Learning (difficulty: hard)" ]
+            --             , li [] [ text "The Clean Coder (difficulty: easy)" ]
+            --             , li [] [ text "Refactoring UI (difficulty: easy)" ]
+            --             ]
+            --         ]
+            --     ]
             ]
-        , Route.Blog__Slug_ { slug = "hello" }
-            |> Route.link [] [ Html.text "My blog post" ]
         ]
     }
+
+
+blog_card :
+    { blog_title : String
+    , blog_description : String
+    , blog_link : String
+    }
+    -> Html (PagesMsg Msg)
+blog_card { blog_title, blog_description, blog_link } =
+    div [ class "space-y-5 shadow-xl p-6 rounded-lg " ]
+        [ h3 [ class "mb-2 text-2xl font-bold tracking-tight text-gray-900 " ] [ text blog_title ]
+        , p [ class "max-w-lg mb-3 font-normal text-gray-700 " ] [ text blog_description ]
+        , a
+            [ href blog_link
+            , target "blank_"
+            , class "inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indgio-800"
+            ]
+            [ text "Read more"
+            , Svg.svg
+                [ attribute "aria-hidden" "true"
+                , SvgAttr.class "w-4 h-4 ml-2 -mr-1"
+                , SvgAttr.fill "currentColor"
+                , SvgAttr.viewBox "0 0 20 20"
+                ]
+                [ path
+                    [ SvgAttr.fillRule "evenodd"
+                    , SvgAttr.d "M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    , SvgAttr.clipRule "evenodd"
+                    ]
+                    []
+                ]
+            ]
+        ]
+
+
+interest_card : { interest_title : String, interest_detail : String } -> Html (PagesMsg Msg)
+interest_card { interest_title, interest_detail } =
+    div
+        [ class "flex flex-col justify-between p-4 leading-normal"
+        ]
+        [ h3
+            [ class "mb-2 text-xl font-bold tracking-tight text-gray-900 "
+            ]
+            [ text interest_title ]
+        , p
+            [ class "mb-3 text-lg font-normal text-gray-700"
+            ]
+            [ text interest_detail ]
+        ]
+
+
+width_height : String
+width_height =
+    "24"
+
+
+github_icon : Html msg
+github_icon =
+    Svg.svg
+        [ SvgAttr.width width_height
+        , SvgAttr.height width_height
+        , SvgAttr.viewBox "0 0 24 24"
+        ]
+        [ path
+            [ SvgAttr.d "M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm2.218 18.616c-.354.069-.468-.149-.468-.336v-1.921c0-.653-.229-1.079-.481-1.296 1.56-.173 3.198-.765 3.198-3.454 0-.765-.273-1.389-.721-1.879.072-.177.312-.889-.069-1.853 0 0-.587-.188-1.923.717-.561-.154-1.159-.231-1.754-.234-.595.003-1.193.08-1.753.235-1.337-.905-1.925-.717-1.925-.717-.379.964-.14 1.676-.067 1.852-.448.49-.722 1.114-.722 1.879 0 2.682 1.634 3.282 3.189 3.459-.2.175-.381.483-.444.936-.4.179-1.413.488-2.037-.582 0 0-.37-.672-1.073-.722 0 0-.683-.009-.048.426 0 0 .46.215.777 1.024 0 0 .405 1.25 2.353.826v1.303c0 .185-.113.402-.462.337-2.782-.925-4.788-3.549-4.788-6.641 0-3.867 3.135-7 7-7s7 3.133 7 7c0 3.091-2.003 5.715-4.782 6.641z"
+            ]
+            []
+        ]
+
+
+section_gradient : String
+section_gradient =
+    "text-transparent bg-clip-text bg-gradient-to-r from-red-400 via-indigo-500 to-red-500"
+
+
+margin_content_component : List (Html (PagesMsg Msg)) -> Html (PagesMsg Msg)
+margin_content_component =
+    div [ class "px-5 md:px-0 md:mx-auto md:max-w-3xl" ]
+
+
+personal_project_component :
+    { a
+        | project_name : String
+        , project_description : Maybe (Html (PagesMsg Msg))
+        , github_link : String
+    }
+    -> Html (PagesMsg Msg)
+personal_project_component comp_data =
+    div [ class "" ]
+        [ div [class "flex space-x-3 justify-between items-center"] 
+            [h3
+                [ class "text-gray-800 font-semibold tracking-wide text-lg"
+                , target "blank_"
+                , href comp_data.github_link
+                ]
+            [ text comp_data.project_name ]
+        , span []
+            [ a 
+                [ href comp_data.github_link
+                , target "blank_"
+                , class "transition duration-200 group rounded-lg "
+                ] [github_icon] 
+            
+            ]
+        ]
+        , case comp_data.project_description of
+            Just desc ->
+                div [ class "text-gray-700 mt-5 " ] [ desc ]
+
+            Nothing ->
+                text ""
+        ]
+
+
+personal_project_list_component : Html (PagesMsg Msg)
+personal_project_list_component =
+    section [ class "mt-32 md:mt-64 " ]
+        [ margin_content_component
+            [ h2 [ class <| section_gradient ++ " text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
+                [ text "Personal Projects" ]
+            , ol [ class "mt-14 grid gap-2 md:grid-cols-2 md:gap-x-2 md:gap-y-4" ]
+                (List.map personal_project_component personal_project_list) 
+                  
+            ]
+        ]
+
+personal_project_list_component2 : Html (PagesMsg Msg)
+personal_project_list_component2 =
+    section [ class "mt-32 md:mt-64 " ]
+        [ margin_content_component
+            [ h2 [ class <| section_gradient ++ " text-indigo-500 text-lg font-semibold tracking-wide uppercase" ]
+                [ text "Personal Projects" ]
+                  
+            ]
+
+            , div [class "mt-10 space-y-5"] 
+                (List.map 
+                    (\proj -> div [] [text proj.project_name]) 
+                    personal_project_list
+                )
+        ]
+
+
+type alias PersonalProject =
+    { project_name : String
+    , project_description : Maybe (Html (PagesMsg Msg))
+    , github_link : String
+    }
+
+
+personal_project_list : List PersonalProject
+personal_project_list =
+    [ { project_description = Just (div [] [ text "Full Stack web app development with: Hasura, Elm, Python, Tailwindcss, Graphql, Vite." ])
+      , project_name = "Custom Tech Stack"
+      , github_link = "https://github.com/Farooq-azam-khan/my_tech_stack_sample"
+      }
+    , { project_name = "Garbage Classification with CNN"
+      , project_description = Nothing
+      , github_link = "https://github.com/Farooq-azam-khan/cps803-course-project"
+      }
+    , { project_name = "Haiku Generator"
+      , project_description = Nothing
+      , github_link = "https://github.com/Farooq-azam-khan/haiku/"
+      }
+    , { project_description = Nothing
+      , project_name = "Jarvis the Typographer"
+      , github_link = "https://github.com/Farooq-azam-khan/Jarvis-the-Typographer"
+      }
+    , { project_description = Nothing, project_name = "D3Js Reference Tutorial", github_link = "https://github.com/Farooq-azam-khan/d3js-tutorials" }
+    , { project_description = Nothing
+      , project_name = "Fourier Analysis Modules"
+      , github_link = "https://github.com/Farooq-azam-khan/fourier-analysis-modules"
+      }
+    , { project_description = Nothing
+      , project_name = "My Sonfigy"
+      , github_link = "https://github.com/Farooq-azam-khan/my-songify"
+      }
+    , { project_description = Just (div [] [ text "Practice making 'twitter-ui', 'twitch-ui', 'spotify-ui'" ])
+      , project_name = "Frontend Practice"
+      , github_link = ""
+      }
+    , { project_name = "Chat App"
+      , project_description = Just (div [] [ text "A Chat application built with elm, expressjs, and websockets." ])
+      , github_link = "https://github.com/Farooq-azam-khan/chat-app-elm"
+      }
+    , { project_name = "Twitch Python Discord Bot"
+      , project_description = Nothing 
+      , github_link = "https://github.com/Farooq-azam-khan/twitch-api-discord-bot"
+      }
+    , { project_name = "Rust Tauri App Practice"
+      , project_description = Nothing 
+      , github_link = "https://github.com/Farooq-azam-khan/color-picker-tauri-test-project"
+      }
+
+    ]
