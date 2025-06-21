8b0daa92093fdd4d189939a4489fbaa5014a1574
farooq-azam-khan <khanazamfarooq99@gmail.com>
2025-06-21 15:33:26 -0400

    fix build

--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -1,4 +1,4 @@
-export default function Marquee({ items }: { items: string[] }) {
+export default function Marquee({ items }: { items: any[] }) {
   return (
     <div className="relative flex w-full overflow-x-hidden border-b-2 border-t-2 border-foreground/50 bg-card text-text text-card-foreground font-base">
       <div className="animate-marquee whitespace-nowrap py-8">
