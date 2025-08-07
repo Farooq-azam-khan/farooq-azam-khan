67c08ddbf7ebedcd74769b68db45aa1677de2721
farooq-azam-khan <khanazamfarooq99@gmail.com>
2025-08-07 19:15:32 -0400

    cool colours for blog cards

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -37,32 +37,46 @@ export function BlogSectionCarousel({ blogs }: { blogs: Blog[] }) {
       }}
     >
       <CarouselContent>
-        {blogs.map((blog, index) => (
-          <CarouselItem
-            key={index}
-            className="flex-none snap-center w-[80%] sm:w-[60%] md:w-[45%] lg:w-[30%]"
-          >
-            <Card className="flex flex-col h-full hover:shadow-lg transform hover:scale-105 transition duration-300">
-              <CardHeader>
-                <CardTitle className="line-clamp-2">{blog.title}</CardTitle>
-                <CardDescription className="line-clamp-3">
-                  {blog.description}
-                </CardDescription>
-              </CardHeader>
-              <CardContent />
-              <CardFooter className="flex items-center justify-between w-full h-full">
-                <LinkButton
-                  href={blog.cta}
-                  target="blank_"
-                  className="inline-flex items-center space-x-3"
-                >
-                  Read more
-                  <MoveRight />
-                </LinkButton>
-              </CardFooter>
-            </Card>
-          </CarouselItem>
-        ))}
+        {blogs.map((blog, index) => {
+          const bgColors = [
+            'bg-purple-200',
+            'bg-yellow-200',
+            'bg-green-200',
+            'bg-pink-200',
+            'bg-indigo-200',
+          ];
+          const num = (index + 1).toString().padStart(2, '0');
+          return (
+            <CarouselItem
+              key={index}
+              className="flex-none snap-center w-[80%] sm:w-[60%] md:w-[45%] lg:w-[30%]"
+            >
+              <Card
+                className={`relative flex flex-col h-full border-4 border-black shadow-[4px_4px_0_rgba(0,0,0,1)] transition-transform duration-200 hover:skew-y-1 hover:shadow-[6px_6px_0_rgba(0,0,0,1)] ${bgColors[index % bgColors.length]}`}
+              >
+                <div className="absolute top-2 left-2 font-mono font-bold text-lg">{num}</div>
+                <CardHeader>
+                  <CardTitle className="text-2xl md:text-3xl lg:text-4xl font-extrabold line-clamp-2">
+                    {blog.title}
+                  </CardTitle>
+                  <CardDescription className="line-clamp-3">
+                    {blog.description}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="flex-grow" />
+                <CardFooter className="w-full">
+                  <LinkButton
+                    href={blog.cta}
+                    target="blank_"
+                    className="w-full py-2 text-center bg-pink-500 text-white font-bold"
+                  >
+                    Read more →
+                  </LinkButton>
+                </CardFooter>
+              </Card>
+            </CarouselItem>
+          );
+        })}
       </CarouselContent>
       <CarouselPrevious disabled={false} />
       <CarouselNext disabled={false} />
